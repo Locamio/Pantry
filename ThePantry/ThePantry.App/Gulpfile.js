2a3b9cbd92bdb8bd93609df2367ad76f0a7f9e48
Daniel Lewis <danlewis.ltd@gmail.com>
2015-06-08 16:07:52 +1200

    add gulp files

---
+++ ThePantry/ThePantry.App/Gulpfile.js
@@ -0,0 +1,34 @@
+﻿var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+var config = {
+    //Include all js files but exclude any min.js files
+    src: ['App/**/*.js', '!App/**/*.min.js'],
+}
+
+// Synchronously delete the output file(s)
+gulp.task('clean', function () {
+    del.sync(['App/all.min.js'])
+});
+
+// Combine and minify all files from the app folder
+gulp.task('scripts', ['clean'], function () {
+    return gulp.src(config.src)
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+      .pipe(gulp.dest('App/'));
+});
+
+//Set a default tasks
+gulp.task('default', ['scripts'], function () { });
+
+var watch = require('gulp-watch');
+gulp.task('watch', function () {
+    return gulp.watch(config.src, ['scripts']);
+});
